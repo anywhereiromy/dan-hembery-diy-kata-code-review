f0060e68b083259c4232aa461a161b4224584823
DanHembery <93157414+DanHembery@users.noreply.github.com>
2021-11-30 12:57:07 +0000

    fizzBuzz complete

--- test/kata1.fizzBuzz.test.js
+++ test/kata1.fizzBuzz.test.js
@@ -1,11 +1,18 @@
 const { fizzBuzz } = require("../src");
 
 describe("fizzBuzz", () => {
-  test("returns Fizz when passed a multiple of 3", () => {});
-
-  test("returns Buzz when passed a multiple of 5", () => {});
-
-  test("returns FizzBuzz when passed a multiple 3 and 5", () => {});
-
-  test("returns the number when it isn't a multiple of 3 or 5", () => {});
+  test("returns Fizz when passed a multiple of 3", () => {
+    expect(fizzBuzz(9)).toEqual('Fizz')
+  });
+  test("returns Buzz when passed a multiple of 5", () => {
+    expect(fizzBuzz(25)).toEqual('Buzz')
+  });
+  test("returns FizzBuzz when passed a multiple 3 and 5", () => {
+    expect(fizzBuzz(15)).toEqual('FizzBuzz')
+    expect(fizzBuzz(60)).toEqual('FizzBuzz')
+  });
+  test("returns the number when it isn't a multiple of 3 or 5", () => {
+    expect(fizzBuzz(17)).toEqual(17)
+    expect(fizzBuzz(8)).toEqual(8)
+  });
 });
