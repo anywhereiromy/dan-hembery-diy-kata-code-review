83fe88746d3fdd077ff8628985b6b976be59c13d
DanHembery <93157414+DanHembery@users.noreply.github.com>
2021-11-30 13:07:33 +0000

    Boolean to word complete

--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,5 +1,9 @@
 const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
-  // how do we create specs again???
+  it('returns Yes if a boolean is true and No if a boolean is false', () => {
+  expect(booleanToWord(true)).toEqual('Yes')
+  expect(booleanToWord(false)).toEqual('No')
+  expect(booleanToWord('maybe')).toEqual('Not a boolean!')
+});
 });
