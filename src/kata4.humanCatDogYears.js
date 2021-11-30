26a9b23f52df09a2100235d031451e7aeb9c4e87
DanHembery <93157414+DanHembery@users.noreply.github.com>
2021-11-30 18:18:40 +0000

    catdogyears complete

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,15 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+   const arr = [];
+    if (number === 1 ){
+       arr.push(1, 15, 15)}
+    else if (number === 2){
+        arr.push(2, 24, 24)}
+    else {
+        let catYears = ((number-2)*4)+24;
+        let dogYears = ((number-2)*5)+24;
+        arr.push(number, catYears, dogYears)
+    };
+    return arr
+   };
 
 module.exports = humanCatDogYears;
--- test/kata3.numberToReversedDigits.test.js
+++ test/kata3.numberToReversedDigits.test.js
@@ -1,5 +1,9 @@
 const { numberToReversedDigits } = require("../src");
 
 describe("numberToReversedDigits", () => {
-  test("returns a reversed array of the number's digits", () => {});
+  test("returns a reversed array of the number's digits", () => {
+    expect(numberToReversedDigits(12345)).toEqual([5,4,3,2,1])
+    expect(numberToReversedDigits(123)).toEqual([3,2,1])
+    expect(numberToReversedDigits(456789)).toEqual([9,8,7,6,5,4])
+  });
 });
