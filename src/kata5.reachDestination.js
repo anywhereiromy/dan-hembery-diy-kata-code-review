aa93cd37579e6fbc60b7d02cf068cc4d4930c3f5
DanHembery <93157414+DanHembery@users.noreply.github.com>
2021-11-30 21:41:23 +0000

    reach destination complete

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,7 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+    let arrivalTime = distance / speed;
+    let roundedArrivalTime = Math.ceil(arrivalTime / 0.5) * 0.5;
+    return (`I should be there in ${roundedArrivalTime} hours.`);
+};
 
 module.exports = reachDestination;
