3aafd8fb856272db24f1267a2ac8bfd273113292
DanHembery <93157414+DanHembery@users.noreply.github.com>
2021-12-06 17:05:55 +0000

    join names complete

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,9 @@
-const joinNames = namesObj => {};
-
+const joinNames = namesObj => {
+    const namesArray = namesObj.map(element => element.name);
+    const lastName = namesArray.pop();
+    const firstNames = namesArray.join(", ");
+  
+  return firstNames.concat(` & ${lastName}`);
+    };
 module.exports = joinNames;
+
