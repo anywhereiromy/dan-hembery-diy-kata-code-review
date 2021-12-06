8ded3e4363c9bbf37db3f905fea603a76fe7bee2
DanHembery <93157414+DanHembery@users.noreply.github.com>
2021-12-06 17:20:57 +0000

    get employee name complete

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,6 @@
-const getEmployerRole = (employeeName, employees) => {};
-
+const getEmployerRole = (employeeName, employees) => {
+        const employeeObject = employees.find(element => element.name === employeeName);
+        return employeeObject.role;
+      };
+      
 module.exports = getEmployerRole;
